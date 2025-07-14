207ad592b76bd4b710fa20240b5cd0a67c40e192
Benz Boonsaree <33254030+BenzPowder@users.noreply.github.com>
2025-07-14 16:54:02 +0700

    edit for cloud

--- app.py
+++ app.py
@@ -407,4 +407,5 @@ if __name__ == '__main__':
         seed_historical_data_to_sheet()
     else:
         print("Google Sheets already has data. Skipping seeding.")
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
